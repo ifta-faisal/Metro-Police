ddc9751adb0b881088a4c5f205ba33a359286ac3
Ifta Faisal <iftafaissal759@gmail.com>
2026-01-04 05:04:22 +0600

    log in page routed

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,24 @@ import Services from './pages/Services'
 import About from './pages/About'
 import News from './pages/News'
 import Contact from './pages/Contact'
+import Login from './pages/Login'   
 
 export default function App() {
   return (
     <Router>
       <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/about" element={<About />} />
         <Route path="/news" element={<News />} />
         <Route path="/contact" element={<Contact />} />
+
+        {/* Login Route */}
+        <Route path="/login" element={<Login />} />
       </Routes>
+
       <Footer />
     </Router>
   )
