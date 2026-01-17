a66b9f64cacc8e47720add8ee4e0f3ed5dbebf3e
Ifta Faisal <117251031+ifta-faisal@users.noreply.github.com>
2026-01-17 20:19:26 +0600

    Updated Risk Map

--- src/pages/CrimeRiskMap.jsx
+++ src/pages/CrimeRiskMap.jsx
@@ -7,7 +7,7 @@ import api from '../utils/api';
 import 'leaflet/dist/leaflet.css';
 import './CrimeRiskMap.css';
 
-// Fix marker icons
+
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
